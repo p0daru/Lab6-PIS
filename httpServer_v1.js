d67bafd7f304541aca0a81c2e0276116a3bbb5dc
p0daru <102858022+p0daru@users.noreply.github.com>
2023-04-25 15:53:35 +0300

    commit-4

--- httpServer_v1.js
+++ httpServer_v1.js
@@ -10,20 +10,3 @@ http
     response.end();
   })
   .listen(3000);
-
-// const http = require("http");
-// http
-//   .createServer(function (request, response) {
-//     response.setHeader("Content-Type", "test/html; charset=utf-8;");
-//     if (request.url === "/home" || request.url === "/") {
-//       response.write("<h2>Home</h2>");
-//     } else if (request.url === "/about") {
-//       response.write("<h2>About</h2>");
-//     } else if (request.url === "/contact") {
-//       response.write("<h2>Contacts</h2>");
-//     } else {
-//       response.write("<h2>Not found</h2>");
-//     }
-//     response.end();
-//   })
-//   .listen(3000);
